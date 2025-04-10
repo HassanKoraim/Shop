33c3417e245f6cfd8e77aa25d971374a95a74626
Hassan Koraim <hassankoraim2@gmail.com>
2025-04-10 14:22:21 +0200

    Add Data

---
+++ Shop.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Web.Models;
+
+namespace Shop.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
