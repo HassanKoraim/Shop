7d4a5b9b974be280a01a0ba85df5704f7dd0498e
Hassan Koraim <hassankoraim2@gmail.com>
2025-04-10 14:21:59 +0200

    Add Controllers

---
+++ Shop.Web/Controllers/CategoryController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Web.Data;
+using Shop.Web.Models;
+
+namespace Shop.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;   
+        }
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+        
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+                if(ModelState.IsValid)
+                {
+                    _context.Categories.Add(category);
+                    _context.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+            return View(category);
+        }
+
+        // Edit
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id != null || id != 0)
+            {
+                var category = _context.Categories.Find(id);
+                if (category != null)
+                {
+                      return View(category);
+                }
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if(id != null || id != 0)
+            {
+                var category = _context.Categories.Find(id);
+                if(category != null)
+                {
+                    _context.Categories.Remove(category);
+                    _context.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return NotFound();
+        }
+
+
+    }
+}
